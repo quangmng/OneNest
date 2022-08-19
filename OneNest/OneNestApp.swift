e7463598f73af642e5689debe351faf98fc687d1
Quang Minh Nguyen <quang@quang.id.au>
2022-08-19 23:11:03 +1000

    Initial Commit

---
+++ OneNest/OneNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneNestApp.swift
+//  OneNest
+//
+//  Created by Quang Minh Nguyen on 19/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct OneNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
